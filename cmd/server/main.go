58a8e3d93635523613c62f0b6900959137b935df
Alexandr Kosygin <a.kosygin@webinar.ru>
2024-08-11 19:11:09 +0400

    Ping

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/kosalnik/keeper/internal/config"
 	"github.com/kosalnik/keeper/internal/log"
+	"github.com/kosalnik/keeper/internal/server"
 	"github.com/urfave/cli/v2"
+	"google.golang.org/grpc"
 )
 
 var Version = "0.0.0"
@@ -15,10 +19,24 @@ func main() {
 		Name:    "GophKeeper server",
 		Version: Version,
 		Usage:   "Run and use",
+		Commands: []*cli.Command{
+			{
+				Name: "serve",
+				Action: func(context *cli.Context) error {
+					cfg := config.NewServer()
+					srv := server.NewGRPCServer(cfg.Listen)
+					if err := srv.Serve(); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
+						return err
+					}
+					return nil
+				},
+			},
+		},
 		Action: func(c *cli.Context) error {
 			fmt.Println("Greetings")
 			return nil
 		},
+		DefaultCommand: "serve",
 	}
 
 	if err := app.Run(os.Args); err != nil {
--- internal/server/auth.go
+++
@@ -1,7 +0,0 @@
-package server
-
-import "github.com/kosalnik/keeper/internal/contract"
-
-type AuthGRPCServer struct {
-	contract.AuthServer
-}
